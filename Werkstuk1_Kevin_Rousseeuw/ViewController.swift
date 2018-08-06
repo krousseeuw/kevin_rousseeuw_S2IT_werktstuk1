2edafba9d8e3650a2a1187f025131653b1c4d317
Emenka <emenka@pims-mbp-2.lan>
2018-08-06 10:16:47 +0200

    Initial Commit

---
+++ Werkstuk1_Kevin_Rousseeuw/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Werkstuk1_Kevin_Rousseeuw
+//
+//  Created by Emenka on 06/08/2018.
+//  Copyright © 2018 Emenka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
